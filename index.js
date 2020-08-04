0bb026800af3ecb8a95fb35dd5837071184c9fd3
Marco Pestrin <pestrinmarco@gmail.com>
2020-08-04 12:38:44 +0200

    readme

--- index.js
+++ index.js
@@ -37,33 +37,3 @@ app.post('/login', authentication.login);
 app.listen(global.EXPRESS_PORT, function () {
   console.log('App listening on port '+ global.EXPRESS_PORT);
 });
-/*
-
-Tipi di interazione:
--condivisione del Video
--mi piace al Video
--commento al Video
--tempo di visualizzazione del video
-
-ogni interazione ha un peso differente che va a determinare
-quanto importante è quel canale e quella tipologia di video
-(in base alla categoria) su quel determinato utente iscritto
-
-viene fatta una preferenza sulle categorie e i canali in base
-alle interazione degli utenti
-
-ogni video tiene conto pure di quante interazioni ha avuto
-in modo da creare un determiato hype in base alla viralità
-che sta subendo
-
-
-TO FIX:
--più azioni insieme se la categoria e/o canale sono nuovi, viene
-letta come dato non presente.
-
-TO DO:
-- flag viralità
-- da mettere a cron hype
-
-*/
-
